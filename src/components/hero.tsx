2600167068fa08e25a278ef93e7eb06aaa843f52
Dhruv Rastogi <140196132+dhruvrastogi1003@users.noreply.github.com>
2025-10-31 01:20:22 +0530

    Updated image in hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -52,7 +52,7 @@ export function Hero() {
              <div className="relative w-[300px] h-[300px]">
                 <div className="relative w-full h-full rounded-full overflow-hidden border-4 border-primary/50 shadow-2xl">
                     <Image
-                        src="https://placehold.co/300x300.png"
+                        src="https://github.com/dhruvrastogi1003/dhruvportfolio.dev/blob/main/.idx/Dhruv-Pic-min.jpg"
                         alt="Dhruv Rastogi"
                         data-ai-hint="professional headshot transparent background"
                         fill
